ffe23dd6325f93566181cd7fbd0c9c2e5e173406
Julian Yap <julianokyap@gmail.com>
2021-10-17 18:10:56 -0700

    Subterfuge NFT handling. Some refactor

--- main.go
+++ main.go
@@ -47,6 +47,19 @@ var (
 				},
 			},
 		},
+		{
+			Name:        "subterfuge",
+			Description: "Retrieves the Subterfuge NFT based on the supplied Token ID",
+			Options: []*discordgo.ApplicationCommandOption{
+
+				{
+					Type:        discordgo.ApplicationCommandOptionInteger,
+					Name:        "tokenid",
+					Description: "Token ID",
+					Required:    true,
+				},
+			},
+		},
 	}
 	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
 		"nception": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
@@ -67,7 +80,6 @@ var (
 			for key, element := range nCeptionNft.Attributes {
 				fields = append(fields, &discordgo.MessageEmbedField{Name: key, Value: element})
 			}
-			// TODO: Owner as a Field and add Fields for attributes
 			msgembed := discordgo.MessageEmbed{
 				Title:       fmt.Sprintf("nCeption NFT #%d", tokenID),
 				URL:         "https://nception.ubiqsmart.com",
@@ -82,6 +94,47 @@ var (
 				Name:        "output.png",
 				ContentType: "image/png",
 				Reader:      bytes.NewReader(nCeptionNft.Picture),
+			}
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Embeds: []*discordgo.MessageEmbed{&msgembed},
+					Files:  []*discordgo.File{&attachment},
+				},
+			})
+		},
+		"subterfuge": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			// NFT handling
+			tokenID := big.NewInt(i.ApplicationCommandData().Options[0].IntValue())
+			var subterfugeNFT nft.NFT
+			var msgformat string
+
+			if tokenID.Cmp(big.NewInt(1)) == -1 || tokenID.Cmp(big.NewInt(8888)) == 1 {
+				msgformat = fmt.Sprintf("Invalid Token ID: %d\n", tokenID)
+			} else {
+				subterfugeNFT = nft.HandleSubterfuge(*RpcURL, tokenID)
+				if subterfugeNFT.Owner.Hex() == "0x0000000000000000000000000000000000000000" {
+					msgformat = "Unclaimed\n"
+				}
+			}
+			var fields []*discordgo.MessageEmbedField
+			for key, element := range subterfugeNFT.Attributes {
+				fields = append(fields, &discordgo.MessageEmbedField{Name: key, Value: element})
+			}
+			msgembed := discordgo.MessageEmbed{
+				Title:       fmt.Sprintf("Subterfuge NFT #%d", tokenID),
+				URL:         "https://subterfuge.ubiqsmart.com",
+				Color:       170,
+				Description: msgformat,
+				Fields:      fields,
+				Image: &discordgo.MessageEmbedImage{
+					URL: "attachment://output.png",
+				},
+			}
+			attachment := discordgo.File{
+				Name:        "output.png",
+				ContentType: "image/png",
+				Reader:      bytes.NewReader(subterfugeNFT.Picture),
 			}
 			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
--- nft/nft.go
+++ nft/nft.go
@@ -9,7 +9,8 @@ import (
 
 	"github.com/JoshVarga/svgparser"
 	"github.com/davidbyttow/govips/v2/vips"
-	n "github.com/ubiq/bishop-discord/contracts"
+	n "github.com/ubiq/bishop-discord/contracts/n"
+	subterfuge "github.com/ubiq/bishop-discord/contracts/subterfuge"
 	"github.com/ubiq/go-ubiq/v5/common"
 	"github.com/ubiq/go-ubiq/v5/ethclient"
 )
@@ -20,10 +21,11 @@ type NFT struct {
 	Picture    []byte
 }
 
-type NceptionURI struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Image       string `json:"image"`
+type GenericSVGURI struct {
+	Name        string              `json:"name"`
+	Description string              `json:"description"`
+	Image       string              `json:"image"`
+	Attributes  []map[string]string `json:"attributes"`
 }
 
 func HandleNception(RpcURL string, TokenID *big.Int) NFT {
@@ -41,7 +43,6 @@ func HandleNception(RpcURL string, TokenID *big.Int) NFT {
 	}
 
 	// Stats
-	// Title "nCeption #x" Details "Owner, Attributes, Picture"
 	var nft NFT
 	ownerOf, err := instance.OwnerOf(nil, TokenID)
 	if err != nil {
@@ -59,10 +60,11 @@ func HandleNception(RpcURL string, TokenID *big.Int) NFT {
 	if err != nil {
 		log.Println(err)
 	}
-	nCeptionURI := decodeNceptionTokenURIbase64(tokenURIbase64)
+	nCeptionURI := decodeGenericSVGTokenURIbase64(tokenURIbase64)
 	nft.Picture = svgToImageLibvips(nCeptionURI.Image)
 
 	// Attributes
+	// nCeption does not have any attributes. We generate a sequence attribute.
 	reader := strings.NewReader(nCeptionURI.Image)
 	e, _ := svgparser.Parse(reader, false)
 	sequence := []string{e.Children[2].Content, e.Children[3].Content, e.Children[4].Content, e.Children[5].Content, e.Children[6].Content, e.Children[7].Content, e.Children[8].Content, e.Children[9].Content}
@@ -75,11 +77,55 @@ func HandleNception(RpcURL string, TokenID *big.Int) NFT {
 	return nft
 }
 
-func decodeNceptionTokenURIbase64(tokenURIbase64 string) NceptionURI {
-	// TODO: Return attributes as well
+func HandleSubterfuge(RpcURL string, TokenID *big.Int) NFT {
+	client, err := ethclient.Dial(RpcURL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer client.Close()
+
+	// subterfuge ERC721 address
+	contractAddress := common.HexToAddress("0xb7F0e951745352a8D641d015DCcBf049F2570027")
+	instance, err := subterfuge.NewSubterfugeCaller(contractAddress, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var nft NFT
+	ownerOf, err := instance.OwnerOf(nil, TokenID)
+	if err != nil {
+		log.Println(err)
+		return nft
+	}
+	nft.Owner = ownerOf
+
+	// Picture
+	tokenURIbase64, err := instance.TokenURI(nil, TokenID)
+	if err != nil {
+		log.Println(err)
+	}
+	subterfugeURI := decodeGenericSVGTokenURIbase64(tokenURIbase64)
+	nft.Picture = svgToImageLibvips(subterfugeURI.Image)
+
+	// Attributes
+	attributes := make(map[string]string)
+	attributes["Owner"] = ownerOf.String()
+	for _, v := range subterfugeURI.Attributes {
+		for kA, vA := range v {
+			if kA == "trait_type" {
+				attributes[vA] = v["value"]
+			}
+		}
+	}
+	nft.Attributes = attributes
+
+	return nft
+}
+
+func decodeGenericSVGTokenURIbase64(tokenURIbase64 string) GenericSVGURI {
 	tokenURI, _ := base64.StdEncoding.DecodeString(strings.TrimPrefix(tokenURIbase64, "data:application/json;base64,"))
 
-	token := NceptionURI{}
+	token := GenericSVGURI{}
 	json.Unmarshal(tokenURI, &token)
 
 	tokenImage, _ := base64.StdEncoding.DecodeString(strings.TrimPrefix(token.Image, "data:image/svg+xml;base64,"))
